0460902f7a085127de79ef1fb4238573c2e71899
Nadav Vanunu <nadav.vanunu@hpe.com>
2018-12-26 14:05:00 +0200

    Version 1 - filtering is working

---
+++ RakBibiFilter/MessageFilterExtension.swift
@@ -0,0 +1,75 @@
+//
+//  MessageFilterExtension.swift
+//  RakBibiFilter
+//
+//  Created by Nadav Vanunu on 26/12/2018.
+//  Copyright © 2018 Nadav Vanunu. All rights reserved.
+//
+
+import IdentityLookup
+
+final class MessageFilterExtension: ILMessageFilterExtension {}
+
+extension MessageFilterExtension: ILMessageFilterQueryHandling {
+    
+    func handle(_ queryRequest: ILMessageFilterQueryRequest, context: ILMessageFilterExtensionContext, completion: @escaping (ILMessageFilterQueryResponse) -> Void) {
+        
+        // First, check whether to filter using offline data (if possible).
+        let offlineAction = self.offlineAction(for: queryRequest)
+        
+        switch offlineAction {
+        case .allow, .filter:
+            // Based on offline data, we know this message should either be Allowed or Filtered. Send response immediately.
+            let response = ILMessageFilterQueryResponse()
+            response.action = offlineAction
+            
+            completion(response)
+            
+        case .none:
+            // Based on offline data, we do not know whether this message should be Allowed or Filtered. Defer to network.
+            // Note: Deferring requests to network requires the extension target's Info.plist to contain a key with a URL to use. See documentation for details.
+            context.deferQueryRequestToNetwork() { (networkResponse, error) in
+                let response = ILMessageFilterQueryResponse()
+                response.action = .none
+                
+                if let networkResponse = networkResponse {
+                    // If we received a network response, parse it to determine an action to return in our response.
+                    response.action = self.action(for: networkResponse)
+                } else {
+                    NSLog("Error deferring query request to network: \(String(describing: error))")
+                }
+                
+                completion(response)
+            }
+        }
+    }
+    
+    private func offlineAction(for queryRequest: ILMessageFilterQueryRequest) -> ILMessageFilterAction {
+        guard let messageBody = queryRequest.messageBody?.lowercased() else { return .none }
+        
+        let defaults = UserDefaults.init(suiteName: "group.rakBibi")
+        let filters = defaults?.stringArray(forKey: "myFilters") ?? [String]()
+        
+        NSLog("Received SMS = \(messageBody)")
+        NSLog("Filters = \(filters)")
+
+        for word in filters {
+            if let value : String = word,
+                messageBody.contains(value) {
+                NSLog("SMS contained bad word -- \(value) -- Filttered!")
+                return .filter
+            }
+        }
+        NSLog("SMS passed the filter")
+        return .allow
+        
+        // Replace with logic to perform offline check whether to filter first (if possible).
+        
+    }
+    
+    private func action(for networkResponse: ILNetworkResponse) -> ILMessageFilterAction {
+        // Replace with logic to parse the HTTP response and data payload of `networkResponse` to return an action.
+        return .none
+    }
+    
+}
--- rakBibi/ViewController.swift
+++ rakBibi/ViewController.swift
@@ -8,13 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var filters : [String]  = []
+    
+    /*
+     // Create and share access to an NSUserDefaults object
+     NSUserDefaults *mySharedDefaults = [[NSUserDefaults alloc] initWithSuiteName: @"com.example.domain.MyShareExtension"];
+     
+     // Use the shared user defaults object to update the user's account
+     [mySharedDefaults setObject:theAccountName forKey:@"lastAccountName"];
+     */
+    
+    func loadSavedFilters() -> Void {
+        let defaults = UserDefaults.init(suiteName: "group.rakBibi")
+        filters = defaults?.stringArray(forKey: "myFilters") ?? [String]()
+    }
+    
+    func saveFilters() -> Void {
+        let defaults = UserDefaults.init(suiteName: "group.rakBibi")
+        defaults?.set(filters, forKey: "myFilters")
+    }
+    
+    func addNewFilter(newFilter : String) -> Void {
+        if (newFilter == "") {
+            return
+        }
+        self.filters.append(newFilter)
+        self.saveFilters()
+        DispatchQueue.main.async { self.tableView.reloadData() }
+    }
+    
+    // MARK: Table
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadSavedFilters()
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count + 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if (indexPath.row == 0) {
+            // addFilterCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "addFilterCell", for: indexPath)
+            
+            return cell
+        }
+        else {
+            //filterCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "filterCell", for: indexPath)
+            
+            cell.textLabel?.text = filters[indexPath.row - 1]
+            
+            return cell
+        }
+        
+    }
+    
+    @IBAction func onAddFilterPressed(_ sender: Any) {
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "Add new filter", message: "Enter a text", preferredStyle: .alert)
+        
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.text = "?"
+        }
+        
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0] // Force unwrapping because we know it exists.
+            let newFilter = textField?.text
+            print("Text field: \(newFilter)")
+            self.addNewFilter(newFilter: newFilter ?? "")
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
